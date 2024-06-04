385a0e7ac1010205811c2cfc2083d9487984c699
GonzaloFernandez20 <lfernandezbogarin@frba.utn.edu.ar>
2024-06-03 22:08:50 -0300

    Proyecto Spring-Maven

---
+++ TP-Anual/tp_anual_implementacion/src/test/java/tp_anual/tp_anual_implementacion/TpAnualImplementacionApplicationTests.java
@@ -0,0 +1,13 @@
+package tp_anual.tp_anual_implementacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpAnualImplementacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
