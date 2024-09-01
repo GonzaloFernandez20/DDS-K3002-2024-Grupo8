da67a523a9839bf40e146381d5ca7cfeec069775
scachero <scachero@frba.utn.edu.ar>
2024-09-01 20:04:16 -0300

    Detalles Colabs y Cuentas

--- TP-Anual/Entrega4/ModificarColaboradorJuridicoCuenta.js
+++ TP-Anual/Entrega4/ModificarColaboradorJuridicoCuenta.js
@@ -1,5 +1,5 @@
-document.getElementById('mod-cuenta').addEventListener('submit', function(event) {
+document.getElementById('mod-cuenta-juridico').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    
+    // Modificar al usuario
 })
--- TP-Anual/Entrega4/ModificarColaboradorUsuario.js
+++ TP-Anual/Entrega4/ModificarColaboradorUsuario.js
@@ -6,3 +6,27 @@ document.querySelectorAll('.baja').forEach(button => {
         }
     });
 });
+
+function habilitarInputIdentificacion(tipo) {
+    if(tipo=="humano") {
+        document.getElementById('nuevoMedioDeContactoHumano').removeAttribute("disabled");
+    } else {
+        document.getElementById('nuevoMedioDeContacto').removeAttribute("disabled");
+    }
+}
+
+document.querySelectorAll('.mod-cuenta').forEach(input => {
+    input.addEventListener('submit', function() {
+        if(!confirm("¿Está seguro que quiere realizar estas modificaciones?")) {
+            return;
+        }    
+    
+        let nombreNuevo = document.getElementsByClassName('mod-cuenta-nombre').value;
+        let contraseniaNueva = document.getElementsByClassName('mod-cuenta-contrasenia').value;
+        let direccionNueva = document.getElementsByClassName('mod-cuenta-direccion').value;
+        let tipoMedioNuevo = document.getElementsByClassName('mod-cuenta-tipo-medio').value;
+        let medioNuevo = document.getElementsByClassName('mod-cuenta-medio').value;
+    
+        // Modificar al usuario
+    })
+})
