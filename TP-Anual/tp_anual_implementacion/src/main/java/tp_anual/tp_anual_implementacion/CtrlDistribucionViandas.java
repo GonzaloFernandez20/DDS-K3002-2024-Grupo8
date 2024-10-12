a2c2bdde16eef79d14328a0419c7e02629463ac9
Caro <ccastellucci@frba.utn.edu.ar>
2024-10-12 12:51:00 -0300

    Controller Distribucion de viandas

--- TP-Anual/tp_anual_implementacion/src/main/java/tp_anual/tp_anual_implementacion/CtrlDistribucionViandas.java
+++ TP-Anual/tp_anual_implementacion/src/main/java/tp_anual/tp_anual_implementacion/CtrlDistribucionViandas.java
@@ -2,9 +2,10 @@ package tp_anual.tp_anual_implementacion;
 
 import DTOs.DistribucionDeViandaDTO;
 import DTOs.HeladeraDTO;
+import Modelo.Dominio.GestionDeContribuciones.GestorDistribucionDeViandas;
 import Modelo.Dominio.colaborador.Colaborador;
-import Modelo.Dominio.contribucion.MotivoDeDistribucion;
 import Modelo.Dominio.contribucion.DistribucionDeVianda;
+import Modelo.Dominio.contribucion.MotivoDeDistribucion;
 import Modelo.Dominio.documentacion.Documento;
 import Modelo.Dominio.documentacion.Sexo;
 import Modelo.Dominio.documentacion.TipoDeDocumento;
@@ -12,12 +13,11 @@ import Modelo.Dominio.heladera.Heladera;
 import Modelo.Dominio.localizacion.Direccion;
 import Modelo.Dominio.medios_de_contacto.WhatsApp;
 import Modelo.Dominio.persona.PersonaHumana;
+import Modelo.Dominio.suscripcion.GestorDeSuscripciones;
 import Repositorios.RepositorioHeladeras;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -29,14 +29,16 @@ public class CtrlDistribucionViandas {
     //COLABORADOR HARDCODEADO HASTA PODER ARMAR LA SESIÓN
     private final Colaborador colaborador = new Colaborador(new PersonaHumana("Luis", "Gómez", LocalDate.now(), new Documento(TipoDeDocumento.DNI, "43.444.444", Sexo.MASCULINO), new Direccion("Saraza", "1200", "1234")), List.of(new WhatsApp("15 2350-2350")));
     //
-    private final List<HeladeraDTO> heladeras = RepositorioHeladeras.getInstancia().getHeladeras().stream().map(heladera -> convertirHeladeraADTO(heladera)).collect(Collectors.toList());;
+    private final List<HeladeraDTO> heladeras = RepositorioHeladeras.getInstancia().getHeladeras().stream().map(this::convertirHeladeraADTO).collect(Collectors.toList());
     //model.addAtribute("heladeras", new DistribucionDeVianda(colaborador, heladeraOrigen, heladeraDestino, MotivoDeDistribucion.FALTA_DE_VIANDAS, 2, LocalDate.now()));
 
     List<MotivoDeDistribucion> motivos = new ArrayList<>();
 
     public void setMotivos() {
-        motivos.add(MotivoDeDistribucion.FALTA_DE_VIANDAS);
-        motivos.add(MotivoDeDistribucion.DESPERFECTO_HELADERA);
+        if(motivos.isEmpty()) {
+            motivos.add(MotivoDeDistribucion.FALTA_DE_VIANDAS);
+            motivos.add(MotivoDeDistribucion.DESPERFECTO_HELADERA);
+        }
     }
 
     @GetMapping("/DistribuirVianda")
@@ -48,20 +50,20 @@ public class CtrlDistribucionViandas {
         return "DistribuirVianda";
     }
 
-    @PostMapping("/DistribuirViandaRespuesta")
-    public String procesarFormulario(@ModelAttribute DistribucionDeViandaDTO distribucionDeViandaDTO) {
-        System.out.println("cantidad de viandas a donar" + distribucionDeViandaDTO.getCantidadDeViandas());
-        DistribucionDeVianda distribucionDeVianda1 = procesarDTO(distribucionDeViandaDTO);
+    @PostMapping("/DistribuirVianda")
+    public String procesarFormulario(@RequestParam(value = "heladeraOrigen", defaultValue = "0") String idHeladeraOrigen,
+                                     @RequestParam(value = "heladeraDestino", defaultValue = "0") String idHeladeraDestino,
+                                     @RequestParam(value = "cantidadViandas", defaultValue = "0") int cantidadViandas,
+                                     @RequestParam(value = "motivoDistribucion", defaultValue = "0") MotivoDeDistribucion motivoDistribucion,
+                                     Model model) {
+        Heladera heladeraOrigen = RepositorioHeladeras.getInstancia().buscarHeladeraPorId(Integer.parseInt(idHeladeraOrigen));
+        Heladera heladeraDestino = RepositorioHeladeras.getInstancia().buscarHeladeraPorId(Integer.parseInt(idHeladeraDestino));
+        DistribucionDeViandaDTO distribucionDeViandaDTO = new DistribucionDeViandaDTO(colaborador, heladeraOrigen, heladeraDestino, motivoDistribucion, cantidadViandas);
+        GestorDistribucionDeViandas.crearContribucion(distribucionDeViandaDTO);
         // agregar a la base de datos
+        model.addAttribute("mensaje", "Distribución realizada con éxito!");
 
-        // Mensaje de exito
-        //model.addAttribute("mensaje", "Distribución realizada con éxito!");
-
-        return "Home";
-    }
-
-    public DistribucionDeVianda procesarDTO(DistribucionDeViandaDTO distribucionDeViandaDTO) {
-        return new DistribucionDeVianda(distribucionDeViandaDTO.getColaborador(), distribucionDeViandaDTO.getHeladeraDeOrigen(), distribucionDeViandaDTO.getHeladeraDestino(), distribucionDeViandaDTO.getMotivoDeDistribucion(), distribucionDeViandaDTO.getCantidadDeViandas(), LocalDate.now());
+        return mostrarFormulario(model);
     }
 
     private HeladeraDTO convertirHeladeraADTO(Heladera heladera) {
