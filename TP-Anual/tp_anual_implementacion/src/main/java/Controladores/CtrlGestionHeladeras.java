156ba85acff67ef12a051ace2b9e4de6c694d2f4
scachero <scachero@frba.utn.edu.ar>
2024-10-22 19:56:27 -0300

    Modificar Heladera

--- TP-Anual/tp_anual_implementacion/src/main/java/Controladores/CtrlGestionHeladeras.java
+++ TP-Anual/tp_anual_implementacion/src/main/java/Controladores/CtrlGestionHeladeras.java
@@ -16,12 +16,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /*  TO DO:
-* - Poder modificar una heladera (/ModificarHeladera)
 * - Poder dar de baja una heladera (/EliminarHeladera)
 * - Tener una forma consistente de que las pantallas que compartan el jurídico y el humano
 * se puedan mostrar igual pero cambiando determinadas partes de la pantalla--> EN CAMINO... YA ESTÁS POR LLEGAR!
@@ -44,7 +45,70 @@ public class CtrlGestionHeladeras {
     }
 
     @PostMapping("/ModificarHeladera")
-    public String modificarHeladera(Model model) {
+    public String modificarHeladera(@RequestParam("mod-heladera-punto-title") int idHeladera,
+                                    @RequestParam(name = "nuevaCiudadHeladera", required = false) String nombreCiudad,
+                                    @RequestParam(name = "nuevaCalleHeladera", required = false) String calle,
+                                    @RequestParam(name = "nuevaAlturaHeladera", required = false) String altura,
+                                    @RequestParam(name = "nuevoCodigoPostal", required = false) String codigoPostal,
+                                    @RequestParam(name = "nuevoModeloHeladera", required = false) String modelo,
+                                    @RequestParam(name = "nuevaTempMax", required = false) Integer tempMax,
+                                    @RequestParam(name = "nuevaTempMin", required = false) Integer tempMin,
+                                    @RequestParam(name = "nuevaCapacidadHeladera", required = false) Integer capacidad,
+                                    Model model) {
+        HeladeraDTO heladeraAModificar = heladeras.stream().filter(heladera -> heladera.getIdHeladera() == idHeladera).findFirst().orElse(null);
+
+        if(Objects.isNull(heladeraAModificar)) {
+            model.addAttribute("mensaje", "Error: No eligió una heladera válida");
+            return mostrarHeladerasYAlertas(model);
+        }
+
+        if(!Objects.isNull(nombreCiudad)) {
+            heladeraAModificar.setCiudad(nombreCiudad);
+        }
+
+        if(!Objects.isNull(calle)) {
+            heladeraAModificar.setCalle(calle);
+        }
+
+        if(!Objects.isNull(altura)) {
+            heladeraAModificar.setAltura(altura);
+        }
+
+        if(!Objects.isNull(codigoPostal)) {
+            heladeraAModificar.setCodPostal(codigoPostal);
+        }
+
+        if(!Objects.isNull(modelo)) {
+            heladeraAModificar.setNombreModelo(modelo);
+        }
+
+        if(!Objects.isNull(tempMax)) {
+            heladeraAModificar.setTempMAXmodelo(tempMax);
+        }
+
+        if(!Objects.isNull(tempMin)) {
+            heladeraAModificar.setTempMINmodelo(tempMin);
+        }
+
+        if(!Objects.isNull(capacidad)) {
+            heladeraAModificar.setCapacidadViandas(capacidad);
+        }
+
+        // RepositorioHeladeras.modificarHeladera(heladeraAModificar);
+        // LO DEBE RECIBIR EL REPOSITORIO PARA ACTUALIZARLO EN LA BD
+
+        model.addAttribute("mensaje", "¡Felicitaciones! Se pudo modificar la información de la heladera exitosamente.");
+
+        System.out.println(heladeraAModificar.getIdHeladera());
+        System.out.println(heladeraAModificar.getCiudad());
+        System.out.println(heladeraAModificar.getCalle());
+        System.out.println(heladeraAModificar.getAltura());
+        System.out.println(heladeraAModificar.getCodPostal());
+        System.out.println(heladeraAModificar.getNombreModelo());
+        System.out.println(heladeraAModificar.getTempMAXmodelo());
+        System.out.println(heladeraAModificar.getTempMINmodelo());
+        System.out.println(heladeraAModificar.getCapacidadViandas());
+
         return mostrarHeladerasYAlertas(model);
     }
 
