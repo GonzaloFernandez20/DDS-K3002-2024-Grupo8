d05e84710f200f9fb2fb4137351ba6a482d9a287
abrilvallecillo <avallecillo@frba.utn.edu.ar>
2024-11-17 19:19:28 -0300

    Update MedioDeContacto.java

--- TP-Anual/tp_anual_implementacion/src/main/java/Modelo/Dominio/medios_de_contacto/MedioDeContacto.java
+++ TP-Anual/tp_anual_implementacion/src/main/java/Modelo/Dominio/medios_de_contacto/MedioDeContacto.java
@@ -1,23 +1,12 @@
 package Modelo.Dominio.medios_de_contacto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import Servicios_Externos_APIs.API.WhatsAppService;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "WhatsApp")
-public class WhatsApp extends MedioDeContacto{
-    @Column(name = "nro_de_telefono")
-    private String nroDeTelefono;
-
-    public WhatsApp() {}
-    public WhatsApp(String nroDeTelefono) {
-        this.nroDeTelefono = nroDeTelefono;
-    }
-
-    @Override
-    public void notificar(String mensaje) {
-        WhatsAppService.sendTextMessage(nroDeTelefono, mensaje);
-    }
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class MedioDeContacto {
+    @Id
+    @GeneratedValue
+    private  Integer id_medio_de_contacto;
+    public void notificar(String mensaje,String userId) {}
 }
