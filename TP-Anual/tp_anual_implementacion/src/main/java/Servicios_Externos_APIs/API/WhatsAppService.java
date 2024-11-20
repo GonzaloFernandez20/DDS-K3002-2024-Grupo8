db20e01d1408946aadda67274f0fb86b87ea9e43
abrilvallecillo <avallecillo@frba.utn.edu.ar>
2024-11-20 16:56:53 -0300

    Update WhatsAppService.java

--- TP-Anual/tp_anual_implementacion/src/main/java/Servicios_Externos_APIs/API/WhatsAppService.java
+++ TP-Anual/tp_anual_implementacion/src/main/java/Servicios_Externos_APIs/API/WhatsAppService.java
@@ -1,23 +1,24 @@
 package Servicios_Externos_APIs.API;
-import okhttp3.MediaType;
+
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 import java.io.IOException;
 
-
 public class WhatsAppService {
 
-    private static final String BASE_URL = "https://api.ultramsg.com/instance19/messages/";
-    private static final String API_TOKEN = "JATafg4422g0K54"; // Token de autenticación
+    private static final String BASE_URL = "https://api.ultramsg.com/100015/messages/chat";
+    private static final String API_TOKEN = "kj35lw02kue0fdtr"; // Token de autenticación
     private static final OkHttpClient CLIENT = new OkHttpClient();
 
-    public static void sendTextMessage(String numero, String message) {
-        // Crear el cuerpo de la solicitud con los parámetros necesarios
-        String parameters = "token=" + API_TOKEN + "&to=" + numero + "&body=" + message;
-        MediaType mediaType = MediaType.get("application/x-www-form-urlencoded");
-        RequestBody body = RequestBody.create(parameters, mediaType);
+    public static void sendTextMessage(String numero) {
+        RequestBody body = new FormBody.Builder()
+                .add("token", API_TOKEN)
+                .add("to", numero) // numero del receptor
+                .add("body", "Hola, soy el NOTIBOT de Diseño de Sistemas") 
+                .build();
 
         // Crear la solicitud HTTP
         Request request = new Request.Builder()
@@ -38,4 +39,3 @@ public class WhatsAppService {
         }
     }
 }
-
