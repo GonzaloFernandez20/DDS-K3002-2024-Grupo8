3d063a9f99d2e46a3be550f6b2116751a32ad4b5
Valentin Pino Suriano <vpinosuriano@frba.utn.edu.ar>
2024-11-29 15:48:00 -0300

    Mas Arreglos Errores Varios

--- TP-Anual/tp_anual_implementacion/src/main/java/Modelo/seguridad/GestorInicioDeSesion.java
+++ TP-Anual/tp_anual_implementacion/src/main/java/Modelo/seguridad/GestorInicioDeSesion.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
 public class GestorInicioDeSesion {
 
     private final UsuariosRepository usuariosRepository;
