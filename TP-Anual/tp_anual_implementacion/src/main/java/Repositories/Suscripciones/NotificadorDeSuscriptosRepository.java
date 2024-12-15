5e6271a1a875724ae9d45bf7554da93afaad20a2
Melina Bugallo <melinabugallo@gmail.com>
2024-12-15 14:02:56 -0300

    arreglos del merge para que compile parte 2

--- TP-Anual/tp_anual_implementacion/src/main/java/Repositories/Suscripciones/NotificadorDeSuscriptosRepository.java
+++ TP-Anual/tp_anual_implementacion/src/main/java/Repositories/Suscripciones/NotificadorDeSuscriptosRepository.java
@@ -4,6 +4,6 @@ import Modelo.Dominio.suscripcion.NotificadorDeSuscriptos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface NotificadorDeSuscriptosRepository extends JpaRepository<NotificadorDeSuscriptos, Integer> {
 }
--- TP-Anual/tp_anual_implementacion/src/main/java/Repositories/Suscripciones/SuscripcionesRepository.java
+++ TP-Anual/tp_anual_implementacion/src/main/java/Repositories/Suscripciones/SuscripcionesRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface SuscripcionesRepository extends JpaRepository<Suscripcion, Integer> {
 
     @Query("SELECT s FROM Suscripcion s WHERE s.suscripto = :colaborador")
